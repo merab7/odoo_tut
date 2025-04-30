271660d97a90cf0912d9d861a710337dd0d37ace
merab7 <merabtodua7@gmail.com>
2025-04-30 18:58:05 +0400

    first commit

---
+++ local-addons/estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name":"Real Estate",
+    "category": "Sales",
+    'data': [
+    'security/ir.model.access.csv',
+    'views/estate_property_views.xml',
+    'views/estate_menu.xml',
+],
+}
